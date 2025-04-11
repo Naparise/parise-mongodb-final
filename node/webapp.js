6517269fdeb0cd12d8a47d0f2c92430f8a61748d
naparise <nathanielparise@gmail.com>
2025-04-10 19:59:58 -0500

    Added express-session

--- node/webapp.js
+++ node/webapp.js
@@ -7,16 +7,25 @@ const path = require('path');
 const test = require('assert');
 
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 
 const routes = require('./routes');
 const initDatabases = require('./dbs');
 
 const app = express();
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: false}));	// Argument parsing tool setup
 app.use(bodyParser.json());
+
+
+app.use(session({
+	resave: true, 
+	saveUninitialized: true, 
+	secret: process.env.COOKIE_SECRET ? process.env.COOKIE_SECRET : 'XCR3rsasa%RDHHH', 
+	cookie: { maxAge: 60000}
+}));
 
 
 // Public resource directory
